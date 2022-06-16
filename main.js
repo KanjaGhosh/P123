51a6dc19af21a3f878b9f99c46d2c4f7c8d89d42
KanjaGhosh <77563350+KanjaGhosh@users.noreply.github.com>
2022-06-16 21:42:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,9 +7,26 @@ function setup()
 {
  canvas=createCanvas(300,300)
  canvas.position(850,250)
+ video=createCapture(VIDEO)
+ video.size(300,300)
+ video.position(250,250)
+
+ poseNet=ml5.poseNet(video, modelLoaded)
+poseNet.on("pose", gotPoses)
 }
 
 function draw()
 {
+ background("grey")
+}
 
+function modelLoaded()
+{console,log("model initialized")}
+
+function gotPoses(error, result)
+{
+ if(error)
+ {console.log(error)}
+ else
+ {console.log(results)}
 }
